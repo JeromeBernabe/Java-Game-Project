7e7ff2b05e8fd66e30691de88017c4db17ef8705
JeromeBernabe <164164021+JeromeBernabe@users.noreply.github.com>
2024-04-23 07:05:03 +0800

    Add files via upload

---
+++ Game/src/App.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setTitle("Game");
+        frame.setSize(1000,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setVisible(true);
+    }
+}
