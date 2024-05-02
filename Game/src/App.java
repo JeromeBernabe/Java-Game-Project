6d6b734939ef1db5e8872557752a7fe2171f54c1
unknown <jerbz1122@gmail.com>
2024-05-02 23:13:57 +0800

    Fixed Issue

--- Game/src/App.java
+++ Game/src/App.java
@@ -7,18 +7,19 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         JFrame frame = new JFrame();
         frame.setTitle("Game");
-        frame.setSize(1000,420);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
+        frame.setResizable(false);
+        GamePanel panel = new GamePanel();
+        frame.add(panel);
+        frame.pack();
+        
         frame.setVisible(true);
-
-        frame.setIconImage(new ImageIcon("Game Assets/Logo.jpg").getImage());
+        frame.setIconImage(new ImageIcon("Assets/Logo.jpg").getImage());
         
-
-        JLabel image = new JLabel();
-        
+        panel.startGameThread();
         
     }
 }
